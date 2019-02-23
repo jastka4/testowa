ad715f7ade8255bdf3b1f6d35717491afa7344c1
Justyna Skalska <justyna_skalska@o2.pl>
2019-02-23 19:16:54 +0100

    Inital commit

---
+++ src/main/java/org/catdatingsystem/CatdatingsystemApplication.java
@@ -0,0 +1,13 @@
+package org.catdatingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatdatingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatdatingsystemApplication.class, args);
+	}
+
+}
